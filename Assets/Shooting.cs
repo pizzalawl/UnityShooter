594d09af20e33631143e911830f4431fde66d3f3
pizzalawl <pizzalawls@gmail.com>
2023-03-06 08:54:34 -0500

    First commit, empty project and .gitignore

---
+++ Assets/Shooting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    public GameObject character;
+    Vector3 offset = new Vector3(0, 0, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            Rigidbody rb = bullet.AddComponent<Rigidbody>() as Rigidbody;
+            rb.velocity = transform.forward * bulletSpeed * Time.deltaTime;
+        }
+        transform.position = character.transform.position
+    }
+}
