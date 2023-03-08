2b28522c0eb72e213a03e6dbcaa07860785c53b5
pizzalawl <pizzalawls@gmail.com>
2023-03-07 19:18:16 -0500

    jump fix

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,34 +6,12 @@ public class Movement : MonoBehaviour
 {
     public float jumpForce =200f;
     public float speed = 3f;
-    public bool touchingG = true;
-    void Start()
-    {
-        touchingG = true;
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        transform.position += new Vector3(xValue, 0, zValue);
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-          Rigidbody rb = GetComponent<Rigidbody>();
-          if(touchingG == true){
-            rb.AddRelativeForce(Vector3.up*jumpForce*Time.deltaTime);
-            touchingG = false;
-          }
-        }
-
-        void OnCollisionEnter(Collision collision)
-        {
-          if(collision.collider.name == "Plane")
-          {
-           touchingG = true;
-          }
-        }
+      Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+      float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+      float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+      transform.position += new Vector3(xValue, 0, zValue);
     }
-}
+
