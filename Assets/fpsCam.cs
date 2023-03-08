1cb76f26a08932bcf48c61fe7d387990dba3db2a
Spucky <126801350+Spucky1@users.noreply.github.com>
2023-03-08 15:47:26 -0500

    fwajdhksf i

--- Assets/fpsCam.cs
+++ Assets/fpsCam.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class fpsCam : MonoBehaviour
 {
+    [SeriliezeField] float sensitivity = 100f;
+    public Transform payerbody;
+    float xRotacion = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,15 @@ public class fpsCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+           
+           float mouseX * Input.GetAxis("Mouse X")* Time.deltaTime* sensitivity;
+           float mouseY * Input.GetAxis("Mouse Y")*Time.deltaTime* sensitivity;
+           payerBody.rotate(Vector3.up * mouseX);
+           
+           xRotacion -= mouseY;
+           
+         X = Mathf.Clamp(xRotacion, -90f, 90f);
+           transform.localRotation = Quanternion.Euler(xRotacion,0f,0f);
+           
     }
 }
