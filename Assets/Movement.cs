9e4ca58ac7659f091e8daf5811318f01c86b5bd7
pizzalawl <pizzalawls@gmail.com>
2023-03-07 19:18:31 -0500

    jump fix

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float jumpForce =200f;
-    public float speed = 3f;
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-      Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-      float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-      float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-      transform.position += new Vector3(xValue, 0, zValue);
-    }
+  public float jumpForce = 200f;
+  public float speed = 3f;
+  public Rigidbody rb;
+  public bool onGround = false;
+  
+  void Start(){
+    rb = GetComponent<Rigidbody>();
+  }
+  
+  // Update is called once per frame
+  void Update()
+  {
+    Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    float zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+    float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+    transform.position += new Vector3(xValue, 0, zValue);
 
+    if(Input.GetButtonDown("Jump") && onGround){
+      rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+      onGround = false;
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision){
+    if(collision.gameObject.name == "Plane"){
+      onGround = true;
+    }
+  }
+}
