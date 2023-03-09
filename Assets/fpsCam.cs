ba384e9f80b013f08d0927b9be35abab0fbd1d5c
pizzalawl <pizzalawls@gmail.com>
2023-03-09 08:42:17 -0500

    goofy ahh fps cam

--- Assets/fpsCam.cs
+++ Assets/fpsCam.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class fpsCam : MonoBehaviour
 {
+    Camera m_MainCamera;
+    public Camera m_SecondaryCamera;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_MainCamera = Camera.main;
+        m_MainCamera.enabled = true;
+        m_SecondaryCamera.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.V) && m_MainCamera.enabled){
+            m_SecondaryCamera.enabled = true;
+            m_MainCamera.enabled = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.V) && m_SecondaryCamera.enabled){
+            m_SecondaryCamera.enabled = false;
+            m_MainCamera.enabled = true;
+        }
     }
 }
